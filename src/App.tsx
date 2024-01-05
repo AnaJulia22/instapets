acf53d87346a15537197c7dec3739d5d9dcc0931
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-05 17:35:18 -0300

    new route to icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
+import IconPage from "./pages/ic";
 
 function HomePage() {
-  return <h1>Welcome to Monitoria de Frontend 2023.1</h1>;
+  return <h1>Welcome to Instapets</h1>;
 }
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/icon" element={<IconPage />} />
       </Routes>
     </>
   );
