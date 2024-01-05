6733a3cd88f1ff312765c543fdcedbe730c42a40
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-05 17:35:42 -0300

    login and signup

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,67 @@
+import "C:/Users/anaju/instapets/src/components/Login/styles.css";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+import { NavLink } from "react-router-dom";
+interface LoginProps {
+  loginType: "login" | "signup" | undefined;
+}
+
+function Login({ loginType }: LoginProps) {
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleChangeLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  };
+
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(login);
+  }, [login, password]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Username:", login);
+    console.log("Password:", password);
+    setLogin("");
+    setPassword("");
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        {loginType === "login" ? <h1>Login</h1> : <h1>Criar conta</h1>}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Login"
+            value={login}
+            onChange={handleChangeLogin}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={handleChangePassword}
+            required
+          />
+          <input
+            type="submit"
+            value={loginType === "login" ? "Entrar" : "Criar"}
+          />
+        </form>
+        {loginType === "login" ? (
+          <NavLink to="/signup">Criar nova conta</NavLink>
+        ) : (
+          <NavLink to="/login">Já tenho uma conta</NavLink>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Login;
