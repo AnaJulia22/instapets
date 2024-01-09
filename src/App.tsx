cdf8530fd4137cd3a18626f8c1a0f904d504d278
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-09 18:40:45 -0300

    feat(homepage): Adiciona botão de login e signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import IconPage from "./pages/ic";
 
 function HomePage() {
-  return <h1>Welcome to Instapets</h1>;
+  return (
+    <>
+      <div className="Home">
+        <h1>Welcome to Instapets</h1> 
+        
+        <NavLink to="/login">
+          <button className="Entrar">Fazer Login</button>
+        </NavLink>     
+        
+        <NavLink to="/signup">
+          <button className="NovaConta">Criar nova conta</button>
+        </NavLink>
+      </div>
+    </>
+  );
 }
 
 function App() {
